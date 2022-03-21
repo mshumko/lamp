a53d1b07e21a3c9cbf123ffd50bb1926bfa4f16b
Mykhaylo Shumko <msshumko@gmail.com>
2022-03-21 10:33:47 -0400

    Cleaned up the trajectory loader and added the L and MLT values to the mapped trajectory.

--- trajectory/map_trajectory.py
+++ trajectory/map_trajectory.py
@@ -16,6 +16,8 @@ def load_trajectory(path, t0=None):
     Loads the csv trajectory file containing at least Time, Lat, Lon, and Alt columns.
     """
     trajectory = pd.read_csv(path, index_col=0)
+    trajectory = trajectory.loc[trajectory.index > 0, :]  # Remove times before launch.
+    trajectory.columns = [column.lstrip() for column in trajectory.columns]  # Clean column names
     
     if t0 is not None:
         if isinstance(t0, str):
@@ -23,7 +25,7 @@ def load_trajectory(path, t0=None):
         trajectory.index = pd.to_timedelta(trajectory.index, unit='second') + t0
     return trajectory
 
-def map_trajectory(trajectory, alt, suffix='nominal'):
+def map_trajectory(trajectory, map_alt):
     """
     Maps the trajectory DataFrame to an altitude.
 
@@ -31,31 +33,32 @@ def map_trajectory(trajectory, alt, suffix='nominal'):
     ----------
     trajectory: pd.DataFrame
         A DataFrame containing Time, Lat_(suffix), Lon_(suffix), and Alt_(suffix).
-    alt: float
+    map_alt: float
         The mapping altitude in kilometers
-    suffix: str
-        The column suffixes to use to index.
     """
-    keys_IRBEM_order = [f'Alt_{alt}km', f'Lat_{alt}km', f'Lon_{alt}km']
-    trajectory[keys_IRBEM_order] = np.nan
+    footprint_keys_irbem_order = [f'Alt_{alt}km', f'Lat_{alt}km', f'Lon_{alt}km']
+    trajectory[['L', 'MLT']] = np.nan
+    trajectory[footprint_keys_irbem_order] = np.nan
     m = IRBEM.MagFields(kext='None')  # IGRF with no extrnal model.
     maginput = None
 
-    for i, row in trajectory.iterrows():
-        if suffix == 'nominal':
-            LLA = {
-                'Time':'2022-03-05T11:30:00', 
-                'x1':row[f'Alt_{suffix}'], 
-                'x2':row[f'Lat_{suffix}'], 
-                'x3':row[f'Lon_{suffix}']
-                }
-        else:
-            raise NotImplementedError
-        footprint_output = m.find_foot_point(LLA, maginput, alt, 0)
-        trajectory.loc[i, keys_IRBEM_order] = footprint_output['XFOOT']
+    for time, row in trajectory.iterrows():
+        position = {
+            'Time':time, 
+            'x1':row[f'Alt'], 
+            'x2':row[f'Lat'], 
+            'x3':row[f'Lon']
+        }
+        # Calculate the footprint
+        footprint_output = m.find_foot_point(position, maginput, alt, 0)
+        trajectory.loc[time, footprint_keys_irbem_order] = footprint_output['XFOOT']
+        # Calculate the L-Shell and MLT
+        mag_coords_output = m.make_lstar(position, maginput)
+        trajectory.loc[time, ['L', 'MLT']] = [mag_coords_output['Lm'][0], mag_coords_output['MLT'][0]]
+
     
     # Replace -1E31 IRBEM errors with np.nan
-    trajectory.loc[trajectory[f'Alt_{alt}km'] < 0, keys_IRBEM_order] = np.nan
+    trajectory.loc[trajectory[f'Alt_{alt}km'] < 0, footprint_keys_irbem_order] = np.nan
     return trajectory
 
 def plot_trajectory(trajectory, alt=None, ax=None, color='k'):
@@ -88,23 +91,18 @@ def plot_trajectory(trajectory, alt=None, ax=None, color='k'):
 if __name__ == '__main__':
     t0 = '2022-03-05T11:27:30'
     data_dir = pathlib.Path(__file__).parents[0]
-    path = data_dir / 'lamp_nominal_trajectory.csv'
+    path = data_dir / 'Range_Hot_MorningOf_030522_Extracted_Jaxa_commentadd.csv'
     trajectory = load_trajectory(path, t0=t0)
 
     alts = np.arange(90, 160, 10)
     for alt in alts:
-        save_path = data_dir / f'lamp_nominal_trajectory_mapped_{alt}_km.csv'
+        save_path = data_dir / f'lamp_actual_trajectory_mapped_{alt}_km.csv'
         mapped_trajectory = map_trajectory(trajectory, alt)
         mapped_trajectory.to_csv(save_path, index_label='Time')
 
-        _, ax = plt.subplots(1, 2, figsize=(10, 5))
-        plot_trajectory(mapped_trajectory, alt=alt, ax=ax)
-        ax[1].set_ylim(0, None)
-        plt.tight_layout()
-        plt.show()
-
-    # _, ax = plt.subplots(1, 2, figsize=(10, 5))
-    # plot_trajectory(trajectory, alt=alt_km, ax=ax)
-    # ax[1].set_ylim(0, None)
-    # plt.tight_layout()
-    # plt.show()
+        # _, ax = plt.subplots(1, 2, figsize=(10, 5))
+        # plot_trajectory(mapped_trajectory, alt=alt, ax=ax)
+        # ax[1].set_ylim(0, None)
+        # plt.tight_layout()
+        # plt.show()
+        pass
